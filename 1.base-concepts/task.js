517a4d4c7d30e8c835af1a272df7e717a3b8cfa0
Anastasia test <gorandi@inbox.ru>
2023-06-03 21:41:09 +0300

    first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let d = (b ** 2) - (4 * a * c);
+	if (d < 0) {
+		arr.push();
+	} else if (d == 0) {		
+		let oneRoot = -b / (2 * a);
+			arr.push(oneRoot);
+	} else if (d > 0) {
+		let firstRoot = (-b + Math.sqrt(d))/(2 * a);
+		let secondRoot = (-b - Math.sqrt(d))/(2 * a);
+			arr.push(firstRoot, secondRoot);		  		
+	} 
+  return arr; 
 }
+  
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
