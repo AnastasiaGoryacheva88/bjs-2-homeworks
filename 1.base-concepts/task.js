2dc4fbb15d3a1fdc6e5a906ecd6077f32694447d
Anastasia test <gorandi@inbox.ru>
2023-06-05 20:23:24 +0300

    first and second tasks

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -15,4 +15,21 @@ function solveEquation(a, b, c) {
   return arr; 
 }
   
-
+function calculateTotalMortgage(percent, contribution, amount, date) {
+  let totalAmount;
+   if (isNaN(percent)) {
+   return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+ } else if (isNaN(contribution)) {
+   return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+ } else if (isNaN(amount)) {
+   return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+ } 	
+ let returnAmountBank = amount - contribution;	
+ let date1 = new Date();	
+ let diff = Date.parse(date) - Date.parse(date1);
+ let creditTerm = Math.ceil(diff / 1000 / 60 / 60 / 24 / 30.5);	
+ let P = percent / 12 / 100;
+ let monthlyFee = returnAmountBank * (P + (P / (((1 + P) ** creditTerm) - 1)));
+     totalAmount = creditTerm * monthlyFee;				
+   return Number(totalAmount.toFixed(2));	
+}
